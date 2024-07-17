e1a37d2f34097473b11da0ab6ea5b94f062d62e7
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-07-17 10:21:21 +0530

    Comment

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -10,7 +10,7 @@ router.route('/transfer').post(vertfyJWT, sendcredit)
 router.route('/history/:_id').get(vertfyJWT,checkStatement)
 
 //check statement on the basis of categories
-router.route('/history').get(vertfyJWT,getCategoryHistory)
+router.route('/history').get(vertfyJWT,getCategoryHistory) //not tested yet
 
 
 export default router
